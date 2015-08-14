837590a66306e0534dd282da34abc02d9df1762d
willian ver valen <vervalenpaiva@gmail.com>
2015-08-14 23:23:06 +0200

    implementation of the chart an local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,11 @@
-angular.module('starter.controllers', [])
+var appCtrl = angular.module('starter.controllers', ['highcharts-ng']);
 
-.controller('DashCtrl', function($scope) {});
+appCtrl.controller('DashCtrl', function($scope, ChartCreate) {
+    $scope.chartConfig = ChartCreate;
+    for(var x = 0; x < 24; x++){
+        $scope.chartConfig.series[0].data.push(Math.random());
+        $scope.chartConfig.series[1].data.push(Math.random());
+    }
+});
+
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,66 @@
-angular.module('starter.services', [])
+var appServ = angular.module('starter.services', []);
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+appServ.factory('localStorage', ['$window', function($window){
+    return {
+        set: function(key, value) {
+            $window.localStorage[key] = value;
+        },
+        get: function(key, defaultValue) {
+            return $window.localStorage[key] || defaultValue;
+        },
+        setObject: function(key, value) {
+            $window.localStorage[key] = JSON.stringify(value);
+        },
+        getObject: function(key) {
+            return JSON.parse($window.localStorage[key] || '{}');
+        },
+        removeItem: function(key){
+            $window.localStorage.removeItem(key);
         }
-      }
-      return null;
-    }
-  };
-});
+
+    };
+}]);
+
+appServ.factory('ChartCreate',[function(){
+    var chart = {
+            title: {
+                text: 'Weekly Smoke Pattern',
+            },
+            xAxis: {
+                title:{
+                    text: 'Hours Of the Day'
+                },
+                categories: ['00', '01', '02', '03', '04', '05',
+                '06', '07', '08', '09', '10', '11' , '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+            },
+            yAxis: {
+                title: {
+                    text: 'cig per hour'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }],
+                min: 0
+            },
+            tooltip: {
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'center',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Base line',
+                data: []
+            }, 
+            {
+                name: 'last 7 days',
+                data: []
+            },
+            ],
+        };
+    return chart;
+}]);
