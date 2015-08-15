e689274d6f9d4047ff66ad635e1697eeae62d5ab
willian ver valen <vervalenpaiva@gmail.com>
2015-08-15 13:18:29 +0200

    implemented basic of the chart update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,40 @@
 var appCtrl = angular.module('starter.controllers', ['highcharts-ng']);
 
-appCtrl.controller('DashCtrl', function($scope, ChartCreate) {
+appCtrl.controller('DashCtrl', function($scope, ChartCreate, pouchService, DataBase) {
     $scope.chartConfig = ChartCreate;
-    for(var x = 0; x < 24; x++){
-        $scope.chartConfig.series[0].data.push(Math.random());
-        $scope.chartConfig.series[1].data.push(Math.random());
+    //pouchService.db.destroy();
+
+    //for(var x = -3; x < 4; x++){
+        //var dat = new Date();
+        //dat = dat.addDays(x);
+        //for(var y = 0; y < 24; y++){
+            //dat = dat.addMinutes(Math.floor((Math.random() * 60) + 30));
+            //DataBase.InsertDate(dat, 'Learn');
+        //}
+    //}
+    
+
+    //for(var x = -3; x < 4; x++){
+        //var dat = new Date();
+        //dat = dat.addDays(x);
+        //for(var y = 0; y < 24; y++){
+            //dat = dat.addMinutes(Math.floor((Math.random() * 60) + 30));
+            //DataBase.InsertDate(dat, 'NotLearn');
+        //}
+    //}
+    
+    function setBaseLine(data){
+        $scope.chartConfig.series[0].data = data;
     }
+    function setMedian(data){
+        $scope.chartConfig.series[1].data = data;
+    }
+
+    DataBase.setChart('Learn', setBaseLine);
+    DataBase.setChart('NotLearn', setMedian);
+    
+
+    DataBase.chartIndex();
 });
 
 
