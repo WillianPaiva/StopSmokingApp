098d9af6cda0b139b4a68f480d70b6cce201a193
willian ver valen <vervalenpaiva@gmail.com>
2015-08-17 12:42:10 +0200

    set up changes and default values

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,6 +24,13 @@ appCtrl.controller('DashCtrl', function($scope, ChartCreate, DataBase, $ionicPla
 });
 
 appCtrl.controller('SettingsCtrl', function($scope, $localStorage){
-    $scope.timeFrame;
-    $scope.price;
+    $scope.timeFrame = parseInt($localStorage.get('timeFrame'));
+    $scope.price = parseInt($localStorage.get('price'));
+    $scope.changeTimeFrame = function(data){
+        $localStorage.set('timeFrame', data);
+    };
+    $scope.changePrice = function(data){
+        $localStorage.set('price', data);
+    };
+    
 });
